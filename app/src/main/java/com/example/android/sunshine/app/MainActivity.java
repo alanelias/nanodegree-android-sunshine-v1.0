bda01d3bd30607d6b0fd80a313d360f890c2be16
Alaa Elias <alaaelias@gmail.com>
2016-04-19 17:47:27 -0700

    Quiz: Move to AsyncTask

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,40 +1,43 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.android.sunshine.app;
 
-import android.content.Context;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Layout;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Adapter;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-import com.example.android.sunshine.app.R;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
     @Override
@@ -44,71 +47,11 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        new MenuInflater(this).inflate(R.menu.main, menu);
-        return (super.onCreateOptionsMenu(menu));
-    }
-
-    /**
-     *  A Placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public ArrayAdapter<String> mForecastAdapter;
-
-        public PlaceholderFragment(){
-
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            // Creating fake list of forecast status to test the adapter
-            String[] forecastArray = {
-                    "Today - Sunny - 88/63",
-                    "Tomorrow - Foggy - 70/40",
-                    "Weds - Cloudy - 72/63",
-                    "Thurs - Asteroids - 75/65",
-                    "Fri - Heavy Rain - 65/56",
-                    "Sat - Help Trapped In Weatherstaltion - 60/51",
-                    "Sun - Sunny - 81/68"
-            };
-
-            // converting the array of string to list
-            List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
-
-            // ArrayAdapter will take the data from a the list and create the the ListView items
-            mForecastAdapter = new ArrayAdapter<String>(
-                    // getting the current context
-                    getActivity(),
-                    // id of list item item layout
-                    R.layout.list_item_forecast,
-                    // id of the text view to populate
-                    R.id.list_item_forecast_textview,
-                    // List of forecast status
-                    forecastArray
-            );
-
-            // getting the root view (fragment_main Layout)
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            // get a refrance to ListView
-            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-
-            // attach adapter to listview
-            listView.setAdapter(mForecastAdapter);
-
-            return rootView;
-        }
     }
 }
