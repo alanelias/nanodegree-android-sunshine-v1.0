0a769cf9bded93e9d8975ba50dd96feb4875b69d
Alaa Elias <alaaelias@gmail.com>
2016-05-05 23:09:10 -0700

    Quiz: Create Your Own View

---
+++ app/src/main/java/com/example/android/sunshine/app/MyView.java
@@ -0,0 +1,28 @@
+package com.example.android.sunshine.app;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by alaaelias on 5/5/16.
+ */
+public class MyView extends View {
+
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
